29bb7d61b790a44dc8f9a29d4d072d049feb28f2
arif1101 <arifurrahmanarif223@gmail.com>
2025-03-22 01:59:35 +0600

    'dataType'

---
+++ dataType.py
@@ -0,0 +1,23 @@
+"""
+x = "Hello World"	str	
+x = 20	int	
+x = 20.5	float	
+x = 1j	complex	
+x = ["apple", "banana", "cherry"]	list	
+x = ("apple", "banana", "cherry")	tuple	
+x = range(6)	range	
+x = {"name" : "John", "age" : 36}	dict	
+x = {"apple", "banana", "cherry"}	set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+x = True	bool	
+x = b"Hello"	bytes	
+x = bytearray(5)	bytearray	
+x = memoryview(bytes(5))	memoryview	
+x = None	NoneType
+"""
+setInt={1,1,2,3,4,3,4}
+setStr = {"apple", "apple","piyara","piyara","lebu"}
+listInt = [1,4,3,2,5,7,10,8]
+listStr = ["apple", "banana","piyara","komla","lebu"]
+print('this is set that provide us all unique value : set',setInt,' || ',setStr)
+print('thsi is list : ',listInt,' || ', listStr)
